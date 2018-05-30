4850559adf2e261d7d95797c23ec5ad779b6063a
Brittany Zhang <brittanyzhang@Brittanys-MacBook-Pro.local>
2018-05-29 21:46:03 -0700

    Finished HW14

--- HW14.R
+++ HW14.R
@@ -1,24 +1,19 @@
 library(moments)
 
 # P1
-prop.test(x = c(0.5, 0.9), n = c(724, 610), conf.level = 0.85, correct = F)
-# prop.test(x = c(0.9, 0.5), n = c(610, 724), conf.level = 0.85, correct = F)
+prop.test(x = c(0.9*610, 0.5*724), n = c(610, 724), conf.level = 0.85, correct = F)
 
 
 # R1
-h_scores <- runif(100, 0, 100)
-ai <- mean(sample(h_scores, 50, F, NULL) > 99)
-
-sample <- sample(h_scores, 50, F, NULL)
-subset(sample, sample > 99)
+mean(runif(50, 0, 100) > 99)
 
 # R2
-hist(replicate(50000, sample(h_scores, 50, F, NULL)))
+hist(replicate(50000, mean(runif(50, 0, 100) > 99)))
 
 # R4
 for (c in 1:99) {
-  # print(mean(sample(h_scores, 50, F, NULL) > c))
-  samp_dis <- sample(h_scores, 50, F, NULL)
-  samp_dis <- subset(samp_dis, samp_dis > c)
-  cat("kurtosis for sampling distribution with c =", c, ":", kurtosis(samp_dis), "\n")
+  sample_dis <- (runif(50, 0, 100) > c)
+  cat("kurtosis for sampling distribution with c =", c, ":", kurtosis(sample_dis), "\n")
 }
+
+
